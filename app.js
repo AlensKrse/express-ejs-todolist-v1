ab5495db5646ddba27cc169bf474708b41d06c61
Alens Krseminskis <alenkrse@gmail.com>
2023-07-07 07:01:38 +0300

    add mongoDB to the app, rework app to multi todo lists

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const _ = require('lodash');
 
 const app = express();
 app.use(express.static("public"));
@@ -9,35 +11,108 @@ app.set('view engine', 'ejs');
 const port = process.env.PORT || 3000;
 const date = require(__dirname + '/date.js');
 
-const items = [];
-const workItems = [];
+const dbUsername = process.env.MONGO_DB_USERNAME || "root";
+const dbPassword = process.env.MONGO_DB_PASSWORD || "root";
+const dbIp = process.env.MONGO_DB_IP || "127.0.0.1";
 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://' + dbUsername + ':' + dbPassword + '@' + dbIp + ':27017');
+}
+
+const itemSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  }
+});
+
+const listItemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  items: [itemSchema]
+});
+
+const Item = mongoose.model("Item", itemSchema);
+const ListItem = mongoose.model("ListItem", listItemSchema);
 
 app.get('/', (req, res) => {
-    const day = date.getDay();
-    res.render('list', { listTitle: day, items: items});
+  const day = date.getDay();
+  Item.find({}).then((items) => {
+    res.render('list', { listTitle: day, items: items });
+  })
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body);
-    const item = req.body.item;
-    if(req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect('/work');
-    } else {
-        items.push(item);
-        res.redirect('/');
-    }
-})
+  const item = req.body.item;
+  const listTitle = req.body.list;
 
-app.get('/work', (req, res) => {
-    res.render('list', { listTitle: 'Work', items: workItems});
+  const newItem = new Item({
+    content: item
+  });
+
+  ListItem.findOne({ title: listTitle }).then((list) => {
+    if (list) {
+      list.items.push(newItem);
+      list.save();
+      res.redirect('/' + listTitle);
+    } else {
+      newItem.save();
+      res.redirect('/');
+    }
+  });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+  res.render('about');
 })
 
+app.post('/delete', (req, res) => {
+  const itemId = req.body.checkbox;
+  const listTitle = req.body.listTitle[0];
+
+  ListItem.findOne({ title: listTitle }).then((list) => {
+    console.log(list);
+    if (list) {
+      list.items.pull({_id: itemId});
+      list.save();
+
+      res.redirect('/' + listTitle);
+    } else {
+      Item.findByIdAndRemove(itemId).then((data) => {
+        console.log('Deleted item : ' + data);
+      });
+
+      res.redirect('/');
+    }
+  });
+})
+
+app.get('/:customListName', (req, res) => {
+  const listName = _.capitalize(req.params.customListName);
+
+  ListItem.findOne({ title: listName }).then((list) => {
+    if (!list) {
+      const newList = new ListItem({
+        title: listName,
+        items: []
+      });
+      newList.save();
+
+      res.redirect('/' + listName);
+    } else {
+      res.render('list', { listTitle: list.title, items: list.items });
+    }
+  });
+});
+
+
+
+
+
 app.listen(port, function () {
-    console.log('Server listening on port: ' + port);
+  console.log('Server listening on port: ' + port);
 })
